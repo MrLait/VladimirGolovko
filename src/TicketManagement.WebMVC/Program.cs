9dcb7a0a36addadfc3b3cb88ec342569ed660946
MrLait <mr.lait3@gmail.com>
2021-05-02 19:37:54 +0300

    Implemented application user controller with Create, Edit and delete users. Implemented 6. f - password modify Implemented roles and role initializer with Admin, eventManager and firstUser

--- src/TicketManagement.WebMVC/Program.cs
+++ src/TicketManagement.WebMVC/Program.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using TicketManagement.WebMVC.Models;
 
 namespace TicketManagement.WebMVC
 {
@@ -9,9 +15,27 @@ namespace TicketManagement.WebMVC
         {
         }
 
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    await RoleInitializerModel.InitializeAsync(userManager, rolesManager);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
